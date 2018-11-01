8065a31d5e97602e56fa3393bdd3ce3f085e8895
Jalaj Punn <jalaj.punn@salesforce.com>
2018-11-01 16:06:14 -0700

    All punctuation is removed from text

---
+++ script.py
@@ -0,0 +1,17 @@
+
+import requests
+import string
+import nltk
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
+r = requests.get('https://medium.com/@jalajpunn/the-struggle-against-the-present-8a3b0a6123c8')
+soup = BeautifulSoup (r.content,features="html5lib")
+
+text = [''.join(s.findAll(text=True)) for s in soup.findAll('p')]
+s = ''.join(text)
+
+words = nltk.word_tokenize(s)
+words = [word.lower() for word in words if word.isalpha()]
+
+print(words)
